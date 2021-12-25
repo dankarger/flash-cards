992f0e7957316008ca3aa873e3ba1fceff7f1337
dankarger <karger.dan@gmail.com>
2021-12-26 00:51:11 +0200

    add readme.md

--- src/components/FlashApp/FlashApp.jsx
+++ src/components/FlashApp/FlashApp.jsx
@@ -5,14 +5,11 @@ import Navbar from "../Navbar/Navbar";
 import ShowCardPage from "../../pages/ShowCardPage/ShowCardPage";
 import HomePage from "../../pages/HomePage/HomePage";
 import ManageCardsPage from "../../pages/ManageCardsPage/ManageCardsPage";
-import {getData} from "../../Data/data";
 
 
 class FlashApp extends React.Component {
 state={Alldata:[]}
- componentDidMount() {
-     // this.setState({Alldata:getData()})
- }
+
 
     render() {
         return (
@@ -25,11 +22,6 @@ state={Alldata:[]}
                         <Route path='/cards' exact element={<ShowCardPage />} />
                         <Route path='/manage' exact element={<ManageCardsPage />} />
                         <Route path='/add' exact element={<ManageCardsPage />} />
-                        {/*<Route path='/products/:id' exact element={<ProductDetailWrapper callback={this.addCart} type='products'/>} />*/}
-                        {/*<Route path='/clothes' exact element={<ClothesPage callback={this.addCart} />} />*/}
-                        {/*<Route path='/clothes/:id' exact element={<ProductDetailWrapper callback={this.addCart}  type='clothes'/>} />*/}
-                        {/*<Route path='/cart' exact element={<Cart cart={this.state.cart}/>} />*/}
-                        {/*/!*<Route path='/create/:id' element={<EditPageWrapper />} />*!/*/}
                     </Routes>
                 </BrowserRouter>
             </div>
