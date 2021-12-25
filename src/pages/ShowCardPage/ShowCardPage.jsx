25de8b2e0ae41176b2f857a2d2beb4444e1cf3ca
dankarger <karger.dan@gmail.com>
2021-12-26 00:04:20 +0200

    arrange code

--- src/pages/ShowCardPage/ShowCardPage.jsx
+++ src/pages/ShowCardPage/ShowCardPage.jsx
@@ -91,7 +91,7 @@ class ShowCardPage extends React.Component {
       shuffle(array) {
         let currentIndex = array.length,  randomIndex;
         // While there remain elements to shuffle...
-        while (currentIndex != 0) {
+        while (currentIndex !== 0) {
             // Pick a remaining element...
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
